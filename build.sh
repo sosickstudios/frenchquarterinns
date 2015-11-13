c1afd9b3a90dc8a965109876197eafc7aed1cd20
Darius Riggins <simovate@gmail.com>
2015-11-12 21:06:05 -0800

    Update and fix build script

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+echo "Building BUILD image"
 docker build -t builder .
+
+cd ../
+
+echo "Starting BUILD container"
+docker run -it --rm -v "$PWD":/frenchquarterinns builder
 
 cd dist/
 
+cd "Building WEBAPP image"
 docker build -t webapp .
 
-docker run -it --rm builder -v "$PWD":/frenchquarterinns
+echo "Starting WEBSERVER container"
 docker run -d --name webserver -p 80:80 webapp
 
 echo curl http://127.0.0.1
